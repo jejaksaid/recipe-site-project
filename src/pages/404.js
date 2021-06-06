1529bb9ec6acef42b95d3edd8c1cb7e0effabba2
jejaksaid <said@fastlearningcenter.com>
2021-06-07 00:00:49 +0700

    initial commit

---
+++ src/pages/404.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Erro = () => {
+  return <div>error page</div>
+}
+
+export default Erro
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/Layout"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <Layout>
+        <h1>home page</h1>
+      </Layout>
+    </div>
+  )
 }
